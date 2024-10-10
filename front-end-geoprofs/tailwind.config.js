ac444b678ebc84e71e75ca962b5d11c30d0d3b74
Rover <117716116+Roverfloats@users.noreply.github.com>
2024-10-10 15:31:48 +0200

    reinstalled everything :(

---
+++ front-end-geoprofs/tailwind.config.js
@@ -0,0 +1,12 @@
+import daisyui from './node_modules/daisyui'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
