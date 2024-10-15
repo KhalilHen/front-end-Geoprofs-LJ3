0143d90b4b4005365bbf5264e8bf9aca26f61e8c
Rover <117716116+Roverfloats@users.noreply.github.com>
2024-10-15 11:36:27 +0200

    more layout work

--- front-end-geoprofs/src/components/FrontPage.jsx
+++ front-end-geoprofs/src/components/FrontPage.jsx
@@ -1,9 +1,35 @@
 function FrontPage() {
     return (
-        <div className="w-full h-[calc(100vh-140px)] flex-col">
-            <div className="w-4/5 h-full bg-[#FF0000]"></div>
-            <div className="w-1/5 h-full bg-[#00FF00]"></div>
+    <div className="w-full h-[calc(100vh-140px)] flex">
+        <div className="w-4/5 h-full bg-[#FF0000]">
+            <div className="w-full h-[200px] bg-[#FF00FF]">
+                <div className="w-full h-1/2 bg-[#FFFF00] flex">
+                    <div className="w-1/4 h-full bg-[#00FFFF]"></div>
+                    <div className="w-3/4 h-full bg-[#FFFF00]"></div>
+                </div>
+                <div className="w-full h-1/2 bg-[#FF00FF] flex">
+                    <div className="w-1/4 h-full bg-[#FFFF00]"></div>
+                    <div className="w-3/4 h-full bg-[#00FFFF]"></div>
+                </div>
+            </div>
+            <div className="w-full h-[calc(100vh-340px)] bg-[#FFFFFF] overflow-y-scroll">
+                <div className="w-full h-[100px] bg-[#FF0000]"></div>
+                <div className="w-full h-[100px] bg-[#0000FF]"></div>
+                <div className="w-full h-[100px] bg-[#FF0000]"></div>
+                <div className="w-full h-[100px] bg-[#0000FF]"></div>
+                <div className="w-full h-[100px] bg-[#FF0000]"></div>
+                <div className="w-full h-[100px] bg-[#0000FF]"></div>
+                <div className="w-full h-[100px] bg-[#FF0000]"></div>
+                <div className="w-full h-[100px] bg-[#0000FF]"></div>
+                <div className="w-full h-[100px] bg-[#FF0000]"></div>
+                <div className="w-full h-[100px] bg-[#0000FF]"></div>
+                <div className="w-full h-[100px] bg-[#FF0000]"></div>
+            </div>
         </div>
+        <div className="w-1/5 h-full bg-[#00FF00]">
+        <div className="w-full h-[100px] bg-[#FF00FF]"></div>
+        </div>
+    </div>
     )
   }
   
