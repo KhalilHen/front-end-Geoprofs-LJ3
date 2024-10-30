372dd81ca348c2952aad3fd97c758cf3d4db6323
Rover <117716116+Roverfloats@users.noreply.github.com>
2024-10-30 13:13:45 +0100

    login page layout is done. no functionality

--- front-end-geoprofs/src/components/Login.jsx
+++ front-end-geoprofs/src/components/Login.jsx
@@ -1,9 +1,13 @@
+import Logo from '../images/GeoprofsLogo.png';
+
 function HeaderNormal() {
 
     return (
       <div className="w-full h-full content-center flex flex-wrap justify-center">
         <div className="bg-[#fff] w-[300px] h-[500px] border-solid border-[#A7A7A7] border-[1px]">
-            <div className="w-full h-[150px]"></div>
+            <div className="w-full h-[150px] content-center flex flex-wrap justify-center">
+                <img className="w-[90%]" src={Logo} alt="" />
+            </div>
             <div className="w-full h-[100px] flex flex-col content-center flex-wrap">
                 <h1 className="text-xl text-center">E-mail / ID</h1>
                 <input className="w-4/5 h-[40px] border-solid border-[#A7A7A7] border-[1px] p-[10px]" type="text" />
