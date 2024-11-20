e546ad5a8f10222acd5a9e2d14b246c9fada89a2
Rover <117716116+Roverfloats@users.noreply.github.com>
2024-11-20 15:47:05 +0100

    finished header wireframe

--- front-end-geoprofs/src/components/HeaderManager.jsx
+++ front-end-geoprofs/src/components/HeaderManager.jsx
@@ -2,23 +2,26 @@ import { Outlet, Link } from "react-router-dom";
 import Logo from '../images/GeoprofsLogo.png';
 import User from '../images/icons/user.png';
 import Notification from '../images/icons/notifications.png';
+import Statistics from '../images/icons/statistics.png';
 
 function HeaderManager() {
   return (
     <header className="flex bg-[#EBEBEB] w-full h-[140px] p-[35px]">
     <div className="bg-[#ff0000] w-[calc(100%/3)] h-full">
-      <Link to="/">
+      <Link to="/front-page">
         <img className="h-full" src={Logo} alt="" />
       </Link>
     </div>
     <div className="bg-[#00ff00] w-[calc(100%/3)] h-full"></div>
     <div className="bg-[#0000ff] w-[calc(100%/3)] h-full flex">
       <div className="w-[50%] h-full bg-[#ff0000] flex justify-between">
-        <button className="bg-[#20B5FF] w-[40px] h-[40px] rounded-full">stat</button>
-        <button className="bg-[#20B5FF] w-[auto] h-[40px] rounded-full flex p-[5px] items-center">
+        <Link to="/statistics-page" className="bg-[#20B5FF] w-[40px] h-[40px] rounded-full flex justify-center items-center ">
+          <img className="w-[30px] h-[30px]" src={Statistics} alt="" />
+        </Link>
+        <Link to="/notification-page" className="bg-[#20B5FF] w-[auto] h-[40px] rounded-full flex p-[5px] items-center">
           <p className="px-[15px] text-white align-middle">Inbox</p>
           <img className="h-[35px] w-[35px]" src={Notification} alt="" />
-        </button>
+        </Link>
       </div>
       <div className="w-[50%] h-full bg-[#ff00ff] flex-row-reverse flex">
         <button className="w-auto h-full flex flex-row-reverse">
