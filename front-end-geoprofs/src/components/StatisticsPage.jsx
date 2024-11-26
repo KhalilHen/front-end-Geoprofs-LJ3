fa3e8bd60916759762ba005da9de27f06cb485ef
Rover <117716116+Roverfloats@users.noreply.github.com>
2024-11-26 11:25:57 +0100

    IvW statistics page: Home button

--- front-end-geoprofs/src/components/StatisticsPage.jsx
+++ front-end-geoprofs/src/components/StatisticsPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Outlet, Link } from "react-router-dom";
 import HeaderNormal from './HeaderNormal'
 import HeaderManager from './HeaderManager'
 
@@ -10,7 +11,22 @@ function StatisticsPage() {
     <>
     <HeaderManager/>
     {/* <HeaderNormal/> */}
- 
+        <div className="w-full h-[calc(100vh-140px)] flex flex-col">
+            <div className='h-[10%] w-full'></div>
+            <div className='h-[70%] w-full flex justify-center'>
+                <div className='h-full w-[80%] bg-[#EBEBEB]'></div>
+            </div>
+            <div className='h-[20%] w-full flex justify-center'>
+                <div className='h-full w-[80%] flex flex-row justify-between items-center'>
+                    <Link to="/front-page" className="border-[#A7A7A7] border-2 bg-[#ffffff] w-[150px] h-[40px] rounded-full flex p-[5px] flex items-center justify-center">
+                        <p>Home</p>
+                    </Link>
+
+
+                </div>
+            
+            </div>
+        </div>
     </>
     )
   }
