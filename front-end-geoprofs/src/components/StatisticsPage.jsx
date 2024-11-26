2f43725b93b5d0ec58cdf5d859d9cbae41b27082
Rover <117716116+Roverfloats@users.noreply.github.com>
2024-11-26 11:06:34 +0100

    US: - Seting up Statistics page:

---
+++ front-end-geoprofs/src/components/StatisticsPage.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import HeaderNormal from './HeaderNormal'
+import HeaderManager from './HeaderManager'
+
+
+
+function StatisticsPage() {    
+
+    return (
+    <>
+    <HeaderManager/>
+    {/* <HeaderNormal/> */}
+ 
+    </>
+    )
+  }
+  
+  export default StatisticsPage 
