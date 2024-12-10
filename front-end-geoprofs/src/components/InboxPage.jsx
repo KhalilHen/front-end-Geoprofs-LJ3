811ac7fa370cd55dcde29801991b770571e64815
Roverfloats <roverfloats@gmail.com>
2024-12-10 11:10:36 +0100

    US: #17.3 - Realizeren: Inbox page: Leave request opener
    did not finish this one, but started it.

--- front-end-geoprofs/src/components/InboxPage.jsx
+++ front-end-geoprofs/src/components/InboxPage.jsx
@@ -78,40 +78,8 @@ function InboxPage(){
             <div class="h-full flex-1 flex items-center flex-col">
                 <p className='text-lg'>Uw open verlof aanvragen</p>
                 <div className='h-full w-[80%]'>
-                <div></div>
-                </div>
-            </div>
-
-            <div class="h-full flex-1 flex items-center flex-col border-solid border-[#EBEBEB] border-r-[2px] border-l-[2px]">
-                <p className='text-lg'>Geschidenis</p>
-                <div className='h-full w-[80%]'>
-                    <select placeholder='Catagorie' className='w-full h-[30px] border-solid border-[#A7A7A7] border-[1px]' name="category" id="category" onChange={handleChangeCategory}>
-                    <option value="" disabled selected hidden>Catagorie</option>
-                    <option>Male</option>
-                    <option>Female</option>
-                    </select>
-                <div></div>
-                </div>
-            </div>
-            {true? 
-            <div class="h-full flex-1 flex items-center flex-col">
-                <p className='text-lg'>Open verlog aanvragen</p>
-                <div className='h-full w-[80%]'>
-                <input placeholder='Name' className='w-full h-[30px] border-solid border-[#A7A7A7] border-[1px]' type="text" id="search" name="search" onChange={handleChangeName}></input>
-                <div></div>
-                </div>
-            </div>
-            :
-            <div class="h-full flex-1 flex items-center flex-col"></div>
-            }
-        </div>
-
-        {/* <div className="w-full h-[calc(100vh-140px)] flex">
-            <div class="flex flex-1 h-full bg-[#ff0000] justify-center">
-                <p className='m-4 text-lg'>Your Open Requests</p>
-                
                 <div>
-                    {leaveOpenRequests.map(leaveRequest => {            
+                {leaveOpenRequests.map(leaveRequest => {            
                         return(
                             <div class="flex">
                                 <LeaveRequest name={leaveRequest.userName} id={leaveRequest.userId}/>
@@ -119,17 +87,18 @@ function InboxPage(){
                         )
                     })}
                 </div>
+                </div>
             </div>
 
-            <div class="flex-1 h-full bg-[#00ff00]">
-                History
-                <select name="category" id="category" onChange={handleChangeCategory}>
-                    <Option name="noChosen" id="-1"/>
-                    <Option name="0" id="0"/>
-                    <Option name="1" id="1"/>
-                    <Option name="2" id="2"/>
-                </select>
-                <div>
+            <div class="h-full flex-1 flex items-center flex-col border-solid border-[#EBEBEB] border-r-[2px] border-l-[2px]">
+                <p className='text-lg'>Geschidenis</p>
+                <div className='h-full w-[80%]'>
+                    <select placeholder='Catagorie' className='w-full h-[30px] border-solid border-[#A7A7A7] border-[1px] mb-[5px]' name="category" id="category" onChange={handleChangeCategory}>
+                    <option value="" disabled selected hidden>Catagorie</option>
+                    <option>Ziek</option>
+                    <option>Vakantie</option>
+                    </select>
+                    <div>
                     {leaveRequestsHistory.map(leaveRequest => {
                         if (filter == -1 || filter == leaveRequest.categoryId){
                             return(
@@ -139,13 +108,15 @@ function InboxPage(){
                             )
                         }
                     })}
+                    </div>
                 </div>
             </div>
-
-
-            <div class="flex-1 h-full bg-[#0000ff]">
-                Leave Requests
-                <input type="text" id="search" name="search" onChange={handleChangeName}></input>
+            {true? 
+            <div class="h-full flex-1 flex items-center flex-col">
+                <p className='text-lg'>Open verlog aanvragen</p>
+                <div className='h-full w-[80%]'>
+                <input placeholder='Name' className='w-full h-[30px] border-solid border-[#A7A7A7] border-[1px] mb-[5px]' type="text" id="search" name="search" onChange={handleChangeName}></input>
+                <div>
                 {leaveRequestsMain.map(leaveRequest => {
                     if (search == "" || leaveRequest.userName.toLowerCase().includes(search.toLocaleLowerCase())){
                         return(
@@ -155,8 +126,13 @@ function InboxPage(){
                         )
                     }
                 })}
+                </div>
+                </div>
             </div>
-        </div> */}
+            :
+            <div class="h-full flex-1 flex items-center flex-col"></div>
+            }
+        </div>
     </>
     )
 }
--- front-end-geoprofs/src/components/LeaveRequest.jsx
+++ front-end-geoprofs/src/components/LeaveRequest.jsx
@@ -1,8 +1,11 @@
+import { Outlet, Link } from "react-router-dom";
+
 function LeaveRequest (props){
     return(
-        <a href={"/leave-request?id="+props.id}>
-            {props.name} Leave Request
-        </a>
+        <Link className=" flex flex-row w-full h-[100px] border-solid border-[#A7A7A7] border-[1px] my-[5px]">
+            <div className="flex flex-1 h-full"></div>
+            <div className="w-[40px] h-fill"></div>
+        </Link>
     )
 }
 
