c6e7aa603b0515b0b54f2c2d29642a037278fdae
Karsten <karstenseijkens@gmail.com>
2025-01-13 22:11:12 +0100

    added GetDepartment

--- front-end-geoprofs/src/App.jsx
+++ front-end-geoprofs/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./components/Login";
 import React, { useState, useEffect } from 'react';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import GetUserDepartment from './components/GetUserDepartments';
 
 function App() {
