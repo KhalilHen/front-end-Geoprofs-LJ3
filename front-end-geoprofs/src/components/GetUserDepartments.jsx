d105c99c415ae2a8325ad179c663093dcc203e23
Karsten <karstenseijkens@gmail.com>
2025-01-24 19:17:19 +0100

    removed duplicated code by putting it in another file where it can be imported from

--- front-end-geoprofs/src/components/GetUserDepartments.jsx
+++ front-end-geoprofs/src/components/GetUserDepartments.jsx
@@ -1,23 +1,8 @@
 import { backendUrl } from '../config/config.json';
 import React, { useState, useEffect } from 'react';
+import { getCookie } from './GetCookie'
 
 function GetUserDepartment(departmentId, setUsers = null, userCookie = null, setResponse = null){
-
-    function getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(';');
-        for(let i = 0; i <ca.length; i++) {
-          let c = ca[i];
-          while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-          }
-        }
-        return "";
-    }
 
     if(!userCookie){
         var userCookie = JSON.parse(getCookie("user"));        
