1c7ae29d88e385d15b5c31fb6ae79651a759e470
Karsten <karstenseijkens@gmail.com>
2025-01-29 22:36:16 +0100

    made get leave request data and made test for it

---
+++ front-end-geoprofs/src/components/GetLeaveRequestData.jsx
@@ -0,0 +1,34 @@
+import { backendUrl } from '../config/config.json';
+import React, { useState, useEffect } from 'react';
+import { getCookie } from './GetCookie'
+
+function GetLeaveRequestData(targetLeaveRequestId, setData = null, userCookie = null, setResponse = null){
+  
+    if(!userCookie){
+        var userCookie = JSON.parse(getCookie("user"));        
+    }
+
+    return fetch(backendUrl+'/getLeaveRequestData?user_id='+userCookie.user_id+'&access_token='+userCookie.access_token+'&cache_id='+userCookie.cache_id+'&leave_request_id='+targetLeaveRequestId, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => {
+        if (setResponse){
+            setResponse(response.status);
+        }
+        if (response.ok) {
+            return response.json();
+        }
+    })
+    .then(async data => {
+        if (setData) {
+            setData(data);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+export { GetLeaveRequestData };
