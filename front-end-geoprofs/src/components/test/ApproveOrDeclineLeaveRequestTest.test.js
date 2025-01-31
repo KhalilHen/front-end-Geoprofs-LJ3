9ca99a2fe85dfa9d79665e86d3f23275d2974b73
Karsten <karstenseijkens@gmail.com>
2025-01-31 14:32:10 +0100

    added some more tests for aprove/decline leave request

--- front-end-geoprofs/src/components/test/ApproveOrDeclineLeaveRequestTest.test.js
+++ front-end-geoprofs/src/components/test/ApproveOrDeclineLeaveRequestTest.test.js
@@ -177,6 +177,116 @@ describe('CEO tries to approves leave request from other with role employee', ()
   } , timeOut);
 });
 
+describe('section manger approves leave request from department manger in section self is managing', () => {
+  test('receives message it was successful', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('1', 'password1', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 7, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  console.log(dataArg);
+
+  expect(responseArg).toBe(200);//response 422 means test may already be run try a fresh migration and seeders before re trying test
+  expect(dataArg.message).toEqual('Leave request successfully approved or declined');
+
+  //TODO test if it is actually changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
+describe('department manger tries to approves leave request from self', () => {
+  test('receives 403', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('2', 'password2', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 7, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  expect(responseArg).toBe(403);
+
+  //TODO test if it is actually didn't changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
+describe('CEO approves leave request from section manger', () => {
+  test('receives message it was successful', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('48', 'password48', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 6, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  console.log(dataArg);
+
+  expect(responseArg).toBe(200);//response 422 means test may already be run try a fresh migration and seeders before re trying test
+  expect(dataArg.message).toEqual('Leave request successfully approved or declined');
+
+  //TODO test if it is actually changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
+describe('section manger tries to approves leave request from self', () => {
+  test('receives 403', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('1', 'password1', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 6, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  expect(responseArg).toBe(403);
+
+  //TODO test if it is actually didn't changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
 //TODO add more tests for when it should fail
 
 //TODO add test for declining leave request
