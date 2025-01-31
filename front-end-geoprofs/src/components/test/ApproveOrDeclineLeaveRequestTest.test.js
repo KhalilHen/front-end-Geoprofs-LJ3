7080894e1b71a89473ab29106aff192d25e68edf
Karsten <karstenseijkens@gmail.com>
2025-01-31 14:05:32 +0100

    added more tests for aprove/decline leave request

--- front-end-geoprofs/src/components/test/ApproveOrDeclineLeaveRequestTest.test.js
+++ front-end-geoprofs/src/components/test/ApproveOrDeclineLeaveRequestTest.test.js
@@ -47,6 +47,136 @@ describe('department manger approves leave request from user in department self 
     } , timeOut);
 });
 
+describe('employee tries to approves leave request from self', () => {
+  test('receives 403', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('3', 'password3', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 1, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  expect(responseArg).toBe(403);
+
+  //TODO test if it is actually didn't changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
+describe('employee tries to approves leave request from other employee', () => {
+  test('receives 403', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('4', 'password4', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 1, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  expect(responseArg).toBe(403);
+
+  //TODO test if it is actually didn't changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
+describe('department manger tries to approves leave request from user in department self is not managing', () => {
+  test('receives 403', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('7', 'password7', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 1, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  expect(responseArg).toBe(403);
+
+  //TODO test if it is actually didn't changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
+describe('section manger tries to approves leave request from other with role employee', () => {
+  test('receives 403', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('1', 'password1', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 1, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  expect(responseArg).toBe(403);
+
+  //TODO test if it is actually didn't changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
+describe('CEO tries to approves leave request from other with role employee', () => {
+  test('receives 403', async () => {
+  const mockSetUser = jest.fn();
+  const mockNavigate = jest.fn();
+  const mockSetResponse = jest.fn();
+  const mockSetData = jest.fn();
+  jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+
+  await Login('48', 'password48', mockSetUser);
+
+  const userArg = mockSetUser.mock.calls[0]?.[0];
+
+  const input = {leave_request_id: 1, value: 2};//value 2 is accept and value 1 would be decline 
+
+  await ApproveOrDeclineLeaveRequest(input , mockSetData, userArg, mockSetResponse);
+
+  const responseArg = mockSetResponse.mock.calls[0]?.[0];
+  const dataArg = mockSetData.mock.calls[0]?.[0];
+
+  expect(responseArg).toBe(403);
+
+  //TODO test if it is actually didn't changes leave request here for need to be GetLeaveRequest made in front end and backend
+
+  } , timeOut);
+});
+
 //TODO add more tests for when it should fail
 
 //TODO add test for declining leave request
